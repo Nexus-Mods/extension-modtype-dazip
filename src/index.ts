d601436ae9d27a1a6e41d7ecdf69a1ce9bf6c2cb
adrian <idc.dts@gmail.com>
2018-11-29 05:06:21 +0000

    -Removed unnecessary dragonage-settings modType; -Modified dazip inner installer to move manifest files within their mod folders during installation to avoid file conflicts

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,22 @@ const app = appIn || remote.app;
 const settingsPath = (game: string) => path.join(app.getPath('documents'), 'BioWare', game, 'Settings');
 
 const DA_CONTENTS_FOLDER = 'contents\\';
+const DA_MODULE_ERF_SUFFIX = '_module.erf';
 
 // Dragon age game information.
 const DA_GAMES = {
   DragonAge1: {
     id: 'dragonage',
     modPath: path.join(app.getPath('documents'), 'BioWare', 'Dragon Age'),
-    settings: settingsPath('Dragon Age'),
 },
   DragonAge2: {
     id: 'dragonage2',
     modPath: path.join(app.getPath('documents'), 'BioWare', 'Dragon Age 2'/*, 'packages', 'core', 'override'*/),
-    settings: settingsPath('Dragon Age II'),
   },
 }
 
 function testDazip(instructions: types.IInstruction[]) {
-  // we can't (currently) now the files are inside a dazip, the outer installer
+  // we can't (currently) know the files that are inside a dazip, the outer installer
   // has to tell us
   return Promise.resolve(false);
 }
@@ -38,7 +37,8 @@ function testSupportedOuter(files: string[]) {
 
 function testSupportedInner(files: string[], gameId: string) {
   const supported = isDragonAge(gameId) 
-    && (files.find(file => file.toLowerCase().indexOf(DA_CONTENTS_FOLDER) === 0) !== undefined)
+    && (files.find(file => file.toLowerCase().indexOf(DA_CONTENTS_FOLDER) !== -1) !== undefined)
+    && (files.find(file => path.basename(file.toLowerCase()).indexOf(DA_MODULE_ERF_SUFFIX) !== -1) !== undefined)
   return Promise.resolve({
     supported,
     requiredFiles: [],
@@ -71,12 +71,21 @@ function installInner(files: string[],
     const result: types.IInstallResult = {
       instructions: []
     };
+
+    let modName = files.find(file => path.basename(file).indexOf(DA_MODULE_ERF_SUFFIX) !== -1);
+    modName = path.basename(modName).replace(DA_MODULE_ERF_SUFFIX, '');
     
     // Go through each file and remove the contents folder.
     files.forEach(file => {
       let newPath = file.toLowerCase();
       if (newPath.indexOf(DA_CONTENTS_FOLDER) !== -1) {
         newPath = newPath.replace(DA_CONTENTS_FOLDER, '');
+      }
+
+      // Move the manifest file from the archive's root to avoid
+      //  mod conflicts.
+      if (newPath === 'manifest.xml') {
+        newPath = newPath.replace(newPath, path.join('AddIns', modName, newPath));
       }
 
       // Ignore any folders as the install manager will
@@ -97,11 +106,6 @@ function isDragonAge(gameId: string): boolean {
   return [ DA_GAMES.DragonAge1.id, DA_GAMES.DragonAge2.id ].indexOf(gameId) !== -1;
 }
 
-function testIsSettings(): Promise<boolean> {
-  // only used for merged files
-  return Promise.resolve(false);
-}
-
 function init(context: types.IExtensionContext) {
   const getPath = (game: types.IGame) => {
     if (game.id === DA_GAMES.DragonAge1.id) {
@@ -111,16 +115,7 @@ function init(context: types.IExtensionContext) {
     }
   };
 
-  const getSettingsPath = (game: types.IGame) => {
-    if (game.id === DA_GAMES.DragonAge1.id) {
-      return DA_GAMES.DragonAge1.settings;
-    } else if (game.id === DA_GAMES.DragonAge2.id) {
-      return DA_GAMES.DragonAge2.settings;
-    }
-  };
-
   context.registerModType('dazip', 25, isDragonAge, getPath, testDazip);
-  context.registerModType('dragonage-settings', 999, isDragonAge, getSettingsPath, testIsSettings);
   context.registerInstaller('dazipOuter', 15, testSupportedOuter, installOuter);
   context.registerInstaller('dazipInner', 15, testSupportedInner, installInner);
 
